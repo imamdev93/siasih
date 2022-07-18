16fb96ad75172817e35ff0d032ae919c4f02afc3
Imam Fahmi <imam.dev93@gmail.com>
2022-07-18 22:09:01 +0700

    jenis absensi

---
+++ app/Enums/KeteranganEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self masuk()
+ * @method static self izin()
+ * @method static self sakit()
+ * @method static self alpa()
+ */
+final class KeteranganEnum extends Enum
+{
+}
