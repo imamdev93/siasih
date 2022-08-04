18e7d2d870e8e96e48143b080dae0ee1f5decf84
Imam Fahmi F <63537236+imamdev93@users.noreply.github.com>
2022-08-04 07:26:37 +0700

    tambah informasi sekolah dan info login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,11 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
+        <div class="alert alert-success">
+            <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
+            Anda Login sebagai <b>{{ session()->get('role') }}</b>
+
+        </div>
         <h2>Beranda</h2>
         <h3>
             Halo, <b>{{ auth()->user()->nama }}</b> !
@@ -29,6 +34,67 @@
             </table>
         </p>
         @endif
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox ">
+                    <div class="ibox-title">
+                        <h5>Informasi Sekolah </h5>
+                    </div>
+                    <div class="ibox-content">
+                        <div class="form-group row mb-1">
+                            <label class="col-lg-2 col-form-label">Nama Sekolah</label>
+                            <div class="col-lg-4 mt-2">
+                                : SD Negeri Sri Asih
+                            </div>
+                            <label class="col-lg-2 col-form-label">NPSN</label>
+                            <div class="col-lg-4 mt-2">
+                                : 20233271
+                            </div>
+                        </div>
+                        <div class="form-group row mb-1">
+                            <label class="col-lg-2 col-form-label">Jenjang Pendidikan</label>
+                            <div class="col-lg-4 mt-2">
+                                : SD
+                            </div>
+                            <label class="col-lg-2 col-form-label">Status Sekolah</label>
+                            <div class="col-lg-4 mt-2">
+                                : Negeri
+                            </div>
+                        </div>
+                        <div class="form-group row mb-1">
+                            <label class="col-lg-2 col-form-label">Alamat Sekolah</label>
+                            <div class="col-lg-4 mt-2">
+                                : Babakan Sawah RT 18/07
+                            </div>
+                            <label class="col-lg-2 col-form-label">Kelurahan</label>
+                            <div class="col-lg-4 mt-2">
+                                : Cidahu
+                            </div>
+                        </div>
+                        <div class="form-group row mb-1">
+                            <label class="col-lg-2 col-form-label">Kecamatan</label>
+                            <div class="col-lg-4 mt-2">
+                                : Pagaden Barat
+                            </div>
+                            <label class="col-lg-2 col-form-label">Kabupaten/Kota</label>
+                            <div class="col-lg-4 mt-2">
+                                : Kab. Subang
+                            </div>
+                        </div>
+                        <div class="form-group row mb-1">
+                            <label class="col-lg-2 col-form-label">Provinsi</label>
+                            <div class="col-lg-4 mt-2">
+                                : Jawa Barat
+                            </div>
+                            <label class="col-lg-2 col-form-label">Negara</label>
+                            <div class="col-lg-4 mt-2">
+                                : Indonesia
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
